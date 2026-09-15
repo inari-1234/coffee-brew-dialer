655eabeaa394787eaa980a2c2c8d42efc3cd33fe
inari-1234 <shouri95@yahoo.co.jp>
2026-09-15 11:09:23 +0900

    Bump PWA cache for v0.2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='coffee-brew-dialer-v0.1.0';
+const CACHE='coffee-brew-dialer-v0.2.0';
 const ASSETS=['./','./index.html','./app.css','./data.js','./app.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
